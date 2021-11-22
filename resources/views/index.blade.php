27ba5abfde8f6fcca187e7de7ebf67e0f55e73b0
DESKTOP-EO39B50\LENOVO PC <niswaayu99@gmail.com>
2021-11-22 14:54:30 +0700

    perubahan ke dua di branch header

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+        <head>
+            <style>
+                body {
+                    background-color: cadetblue;
+                    margin: 250 auto;
+                }
+                H1 {text-align: center;
+                font-size: 40pt;
+                margin: auto;
+                color: aqua;}
+                P {text-align: center;
+                line-height: 2;
+                margin: auto;}
+            </style>
+            <title>percobaan pertama</title>
+        </head>
+    
+        <body>
+            <h1>SELAMAT DATANG</h1>
+            <P>TERUSLAH MENCOBA SAMPAI KAMU SUDAH MERASA PUAS AKAN HASIL YANG ADA</b> </P>
+            <h1>penggunaan merge</h1>
+    
+    
+        </body>
+    </html>
+</body>
+</html>
